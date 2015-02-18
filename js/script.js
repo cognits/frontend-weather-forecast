16dafc775b45c26df4de4d88d5b0f0cdd06ef5dc
KAR2014 <kherrera16@gmail.com>
2015-02-18 11:51:11 -0600

    Agregue un Json de Paises y cree mi clave del weather, agregue una carpeta de imagenes para un mejor entrorno visual

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
 // Javascript Code.
+//Variables
+var pais = "";
+var codigo = "";
+var url = "";
+var display = "";
+var far = "";
+
+$(document).ready(function() {
+
+	$(this).load("paises.json", function(data){
+    var paises = JSON.parse(data);
+
+    pais = paises[0]["name"];
+    codigo = paises[0]["code"];
+    dir = "http://api.wunderground.com/api/8b7e28d768cfa522/conditions/q/"+ codigo +"/"+ pais +".json";
+
+    $('#boton1').click(function() {
+        alert("Weit!!!");
+        $('.screen').append("<p class =\"pai\"> Pais : "+ pais +"</p>");
+        $('.screen').append("<p class =\"cod\"> Codigo: "+ codigo +"</p>");
+  	});
+   	
+
+    });
+    
+    $(this).load(dir, function(data){
+    var clima = JSON.parse(data);
+
+    display = clima["current_observation"]["display_location"]["full"];
+   	far = clima["current_observation"]["temp_f"];
+
+
+    $('#boton2').click(function() {
+        alert("Weit!!!");
+        $('.screen').append("<p class =\"display\"> Pais : "+ display +"</p>");
+        $('.screen').append("<p class =\"f\"> Temperatura °F: "+ far +" °F</p>");
+  	});
+
+
+	});
+ });
