b5604252fe52ba53ae33e8f83f401e88c5e17c3f
KAR2014 <kherrera16@gmail.com>
2015-02-24 15:03:49 -0600

    VF

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ var wind = "";
 var humidity = "";
 var pressure ="";
 var visibility ="";
+var error ="";
 
 
 /*  Variables de los Radio Buttons */
@@ -30,9 +31,12 @@ $(document).ready(function() {
 
     $(".insert").hide("slide");
     $(".screen2").hide("slide");
+    $(".country").val("");
+    $(".city").val("");
 
     //Button Start
     $('#start').click(function() {
+
         country = $("input[name=pais]").val();
         city = $("input[name=ciudad]").val();
 
@@ -50,16 +54,19 @@ $(document).ready(function() {
             };
 
             console.log(pais);
-            console.log(codigo);
-            url = "http://api.wunderground.com/api/8b7e28d768cfa522/conditions/q/"+codigo+"/"+city+".json";
+            console.log(code);
+
+            url = "http://api.wunderground.com/api/8b7e28d768cfa522/conditions/q/"+code+"/"+city+".json";
+
 
             //After of press the buton, he hide 
-            $('#start').hide();
 
             //Display the Country and Code
             $('.screen1').append("<p class =\"pai\">  * Pais : "+ pais +"</p>");
             $('.screen1').append("<p class =\"cod\">  * Codigo: "+ code +"</p></br>");
+
             console.log(url);
+            $('#start').hide();
 
             if (code.length != 0){
                 jQuery(document).ready(function($) { 
@@ -68,67 +75,78 @@ $(document).ready(function() {
                         dataType : "jsonp",
                         success : function(parse_clima) {
 
-                            //Variables
-                            display = parse_clima["current_observation"]["display_location"]["full"];
-                            up_date = parse_clima["current_observation"]["local_time_rfc822"];
-                            weather = parse_clima["current_observation"]["weather"];
-                            image = parse_clima["current_observation"]["icon_url"];
-                            icon = parse_clima["current_observation"]["icon"];
-                            far = parse_clima["current_observation"]["temp_f"];
-                            celcios = parse_clima["current_observation"]["temp_c"];
-                            wind = parse_clima["current_observation"]["wind_string"];
-                            humidity = parse_clima["current_observation"]["relative_humidity"];
-                            pressure = parse_clima["current_observation"]["pressure_mb"];
-                            visibility = parse_clima["current_observation"]["visibility_km"];
-
-
-                            //Add the Div Screen 2
-                            $('.screen2').append("<p class =\"display\">"+ display +"</p><hr>");
-                            
-                            /* Logo Cognits */
-                            $('.screen2').append("<img class =\"cloud\" src= \""+ image +"\" alt =\"Imagen Clima\">");
-                            $('.screen2').append("<p class =\"icon\">"+ icon.toUpperCase() +"</p></br><hr>");
-
-                            if (radio_c == 1){
-                                /* Celcious Temperature */
-                                $('.screen2').append("<p class =\"temp_c_f\"> Temperatura °C:</p> ");
-                                $('.screen2').append("<p class =\"t_c_f\">"+celcios+" °C</p>");}
-                            else if (radio_f == 1){
-                                /*Farenheight Temperature */
-                                $('.screen2').append("<p class =\"temp_c_f\"> Temperatura °F:</p>");
-                                $('.screen2').append("<p class =\"t_c_f\">"+far+" °F</p>");
+                            //Try for error conprovation
+                            try {
+                                error = parse_clima["response"]["error"]["type"];
+                                if (error == "querynotfound"){
+                                    alert("Json Not Found");
+                                }
                             }
-                            /* up_date */
-                            $('.screen2').append("<p class =\"up_date\"> Last Up Date: "+ up_date +"</p>");
-                            
-                            /*Weather*/
-                            $('.screen2').append("<p class =\"weather\"> Weather: "+ weather +"</p>");
-
-                            /*Wind*/
-                            $('.screen2').append("<p class =\"wind\"> Wind : "+ wind +"</p>");
-
-                            /*Humidity*/
-                            $('.screen2').append("<p class =\"humidity\"> Humidity : "+ humidity +"</p>");
-
-                            /*Preasure*/
-                            $('.screen2').append("<p class =\"pressure\"> Pressure : "+ pressure +" hPa</p>");
-
-                            /*Visibility*/
-                            $('.screen2').append("<p class =\"visibility\"> Visibility : "+ visibility +" Km</p>");
-
-                            /** Show my Weather Div */
-                            $(".screen2").show("slide");
-                            /* Clean my Textbox */
-                            $(".country").val("");
-                            $(".city").val("");
+                            catch(TypeError) {
+                                //Variables
+                                display = parse_clima["current_observation"]["display_location"]["full"];
+                                up_date = parse_clima["current_observation"]["local_time_rfc822"];
+                                weather = parse_clima["current_observation"]["weather"];
+                                image = parse_clima["current_observation"]["icon_url"];
+                                icon = parse_clima["current_observation"]["icon"];
+                                far = parse_clima["current_observation"]["temp_f"];
+                                celcios = parse_clima["current_observation"]["temp_c"];
+                                wind = parse_clima["current_observation"]["wind_string"];
+                                humidity = parse_clima["current_observation"]["relative_humidity"];
+                                pressure = parse_clima["current_observation"]["pressure_mb"];
+                                visibility = parse_clima["current_observation"]["visibility_km"];
 
 
+                                //Add the Div Screen 2
+                                $('.screen2').append("<p class =\"display\">"+ display +"</p><hr>");
+                                
+                                /* Logo Cognits */
+                                $('.screen2').append("<img class =\"cloud\" src= \""+ image +"\" alt =\"Imagen Clima\">");
+                                $('.screen2').append("<p class =\"icon\">"+ icon.toUpperCase() +"</p></br><hr>");
+
+                                if (radio_c == 1){
+                                    /* Celcious Temperature */
+                                    $('.screen2').append("<p class =\"temp_c_f\"> Temperatura °C:</p> ");
+                                    $('.screen2').append("<p class =\"t_c_f\">"+celcios+" °C</p>");}
+                                else if (radio_f == 1){
+                                    /*Farenheight Temperature */
+                                    $('.screen2').append("<p class =\"temp_c_f\"> Temperatura °F:</p>");
+                                    $('.screen2').append("<p class =\"t_c_f\">"+far+" °F</p>");
+                                }
+                                /* up_date */
+                                $('.screen2').append("<p class =\"up_date\"> Last Up Date: "+ up_date +"</p>");
+                                
+                                /*Weather*/
+                                $('.screen2').append("<p class =\"weather\"> Weather: "+ weather +"</p>");
+
+                                /*Wind*/
+                                $('.screen2').append("<p class =\"wind\"> Wind : "+ wind +"</p>");
+
+                                /*Humidity*/
+                                $('.screen2').append("<p class =\"humidity\"> Humidity : "+ humidity +"</p>");
+
+                                /*Preasure*/
+                                $('.screen2').append("<p class =\"pressure\"> Pressure : "+ pressure +" hPa</p>");
+
+                                /*Visibility*/
+                                $('.screen2').append("<p class =\"visibility\"> Visibility : "+ visibility +" Km</p>");
+
+                                /** Show my Weather Div */
+                                $(".screen2").show("slide");
+                                /* Clear my Textbox */
+                                $(".country").val("");
+                                $(".city").val("");
+
+                                /*Clear my radiobutton*/
+                                $('input[name=celsius]').attr('checked',false);
+                                $('input[name=fahrenheit]').attr('checked',false);
+                            }
                         }
                     });
                 });
             }
             else{
-                alert("Error Don't find Json");
+                alert("Code Contry not Exist");
             }
         });
 
@@ -141,12 +159,14 @@ $(document).ready(function() {
     $('.r1').click(function(){
         radio_c = 1;
         radio_f = 0;
+        $('input[name=fahrenheit]').attr('checked',false);
     });
 
     //Radio Fahrenheit
     $('.r2').click(function(){
         radio_c = 0;
         radio_f = 1;
+        $('input[name=celsius]').attr('checked',false);
     });
 
     //Text Press for Search
@@ -157,6 +177,8 @@ $(document).ready(function() {
     
     //Button Delete
     $('#dell').click(function() {
+        $('input[name=celsius]').attr('checked',false);
+        $('input[name=fahrenheit]').attr('checked',false);
         $(".country").val("");
         $(".city").val("");
         location.reload();
