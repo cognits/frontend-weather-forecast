26dd419a2abf7671ef97563f233cfc9f7fd5086a
Marvin Abel <marvinabelmon@gmail.com>
2015-03-31 17:03:22 -0600

    En esta  version se termina el diseño de la pagina, se agrega la libreria jquery y css bootstrap, tambien se inicia con la api weather

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 // Javascript Code.
+jQuery(document).ready(function($) {
+  $.ajax({
+  url : "http://api.wunderground.com/api/0d95ff1db656d6bf/geolookup/conditions/q/IA/Cedar_Rapids.json",
+  dataType : "jsonp",
+  success : function(parsed_json) {
+  var location = parsed_json['location']['city'];
+  var temp_f = parsed_json['current_observation']['temp_f'];
+  alert("Current temperature in " + location + " is: " + temp_f);
+  }
+  });
+});
+
