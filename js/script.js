428a19054de8eaf6fa91344e5deeb4f91f135cdd
Marvin Abel <marvinabelmon@gmail.com>
2015-04-06 15:25:08 -0600

    Proyecto terminado con los requerimentos minimos

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ jQuery(document).ready(function($) {
         var country = $("input[name=country]").val();
         var province = $("input[name=province]").val();
         country = country.toLowerCase();
-        var code_country = ""
+        var temp_type = $("input[name=temperature]:checked").val();
+        var code_country = "";
 
         if (country != "" && province != "") {
             $.ajax({
@@ -16,15 +17,26 @@ jQuery(document).ready(function($) {
                     };
                 };
                 if (code_country.length === 0) {
-                    alert("No existe")
+                    alert("Sorry, we have no information of this country.")
                 } else {
                     $.ajax({
                     url : "http://api.wunderground.com/api/0d95ff1db656d6bf/geolookup/conditions/q/"+code_country+"/"+province+".json",
                     dataType : "jsonp",
                     success : function(parsed_json) {
-                    var location = parsed_json['location']['city'];
-                    var temp_f = parsed_json['current_observation']['temp_f'];
-                    alert("Current temperature in " + location + " is: " + temp_f);
+                        console.log(parsed_json["response"]["error"])
+                        if (parsed_json["response"]["error"]) {
+                            alert("Sorry, " + parsed_json["response"]["error"]["description"])
+                        } else {
+                            var location = parsed_json['location']['city'];
+                            if (temp_type === "Fahrenheit") {
+                                var temp_f = parsed_json['current_observation']['temp_f'];
+                                alert("Current temperature in " + location + " is: " + temp_f);
+                            } else {
+                                var temp_c = parsed_json['current_observation']['temp_c'];
+                                alert("Current temperature in" + location + "is" + temp_c);
+                            };
+                        };
+                        console.log(parsed_json)
                     }
                     });
                         };
