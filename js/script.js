54c33eb1aa5ceeb8bafc452423439d3817e26b76
Marvin Abel <marvinabelmon@gmail.com>
2015-04-06 15:45:20 -0600

    En esta version se añaden comentarios, se añade la informacion del clima, y se cambia el icono de la pagina

--- js/script.js
+++ js/script.js
@@ -7,15 +7,18 @@ jQuery(document).ready(function($) {
         var temp_type = $("input[name=temperature]:checked").val();
         var code_country = "";
 
+        /*fields full*/
         if (country != "" && province != "") {
             $.ajax({
             url: "countries.json", dataType: "json",
             success : function(parsed_json) {
+                /*Get country code*/
                 for (var i=0; i <= 242; i++) {
                     if (parsed_json[i]["name"].toLowerCase() === country) {
                         code_country = parsed_json[i]["code"]
                     };
                 };
+
                 if (code_country.length === 0) {
                     alert("Sorry, we have no information of this country.")
                 } else {
@@ -28,12 +31,15 @@ jQuery(document).ready(function($) {
                             alert("Sorry, " + parsed_json["response"]["error"]["description"])
                         } else {
                             var location = parsed_json['location']['city'];
+                            var weather = parsed_json["current_observation"]["weather"]
+
+                            /*Type of temperature*/
                             if (temp_type === "Fahrenheit") {
                                 var temp_f = parsed_json['current_observation']['temp_f'];
-                                alert("Current temperature in " + location + " is: " + temp_f);
+                                alert("Current temperature in " + location + " is: " + temp_f + ". " + weather);
                             } else {
                                 var temp_c = parsed_json['current_observation']['temp_c'];
-                                alert("Current temperature in" + location + "is" + temp_c);
+                                alert("Current temperature in" + location + "is" + temp_c + ". " + weather);
                             };
                         };
                         console.log(parsed_json)
