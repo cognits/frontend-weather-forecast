65d96c30d8998cdd2f56ffbe591a3253d4b9c9d6
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-04-09 11:00:38 -0600

    ya esta casi listo solo falta mejorar las alertas de error de la pagina

--- js/script.js
+++ js/script.js
@@ -12,10 +12,13 @@
     }
 var parse2="";
 jQuery(document).ready(function($) {
+$('.fahrenheit').click(function() {
+    var state = $("input[name=State]").val();
+    var parse2="";
+});
 //The functionality of page
     $('#search').click(function() {
 var codeCountrie="";
-var nameState="";
 var mode_temp = $("input[name=temperature]:checked").val();
         country = $("input[name=Countrie]").val();
         state = $("input[name=State]").val();
@@ -32,44 +35,37 @@ var mode_temp = $("input[name=temperature]:checked").val();
                 };
                 console.log(codeCountrie);
             };
-            for (var s=0; s <= 242; s++) {
-           if (parsed_json1[s]["name"].toLowerCase() === state) {
-                    parse2=parsed_json1[s]["name"]
-                };
-                console.log(parse2);
-              };
             if (codeCountrie.length===0) {
-                alert("Sorry there is no record of what calls");
-            } else if (parse2.length===0){
-                alert("hola que hace");
-            }else{
-              $('#search').empty()
-                  //the efect of slide 
+                if ($(".error").length===0){
+                    $('.error').append("<p class =\"letererror\">You must enter a valid country and a state ....!</p>");
+                }
+            } else{
               $.ajax({
               url : "http://api.wunderground.com/api/f7b861602853b78f/geolookup/conditions/q/"+codeCountrie+"/"+state+".json",
               dataType : "jsonp",
               success : function(parsed_json) {
-
-
-
-
-
-
-
-
-
-              var location = parsed_json['location']['city'];
-              var temp_f = parsed_json['current_observation']['temp_f'];
-              var hcountrie = parsed_json["current_observation"]["display_location"]["full"];
-              var hcity = parsed_json["current_observation"]["display_location"]["country"];
-              var hstate = parsed_json["current_observation"]["display_location"]["state_name"];
-              var weather = parsed_json["current_observation"]["weather"];
-              var dewpoint = parsed_json["current_observation"]["dewpoint_string"];
-              var icon = parsed_json["current_observation"]["icon_url"];
-              var update = parsed_json["current_observation"]["observation_time"];
-              var visibility = parsed_json["current_observation"]["visibility_km"];
-              var humidity = parsed_json["current_observation"]["relative_humidity"];
-              var wind = parsed_json["current_observation"]["wind_kph"];
+                    if (parsed_json["response"]["error"]) {
+                        if ($(".country").length === 0) {
+                            if ($(".error").length===0){
+                                $('.error').append("<p class =\"letererror\">Your search yielded no results try again ....</p>");
+                            };
+                        };
+                    };
+                    console.log(parse2);
+                    console.log(state);
+                if ($(".weather").length === 0) {
+                  var location = parsed_json['location']['city'];
+                  var temp_f = parsed_json['current_observation']['temp_f'];
+                  var hcountrie = parsed_json["current_observation"]["display_location"]["full"];
+                  var hcity = parsed_json["current_observation"]["display_location"]["country"];
+                  var hstate = parsed_json["current_observation"]["display_location"]["state_name"];
+                  var weather = parsed_json["current_observation"]["weather"];
+                  var dewpoint = parsed_json["current_observation"]["dewpoint_string"];
+                  var icon = parsed_json["current_observation"]["icon_url"];
+                  var update = parsed_json["current_observation"]["observation_time"];
+                  var visibility = parsed_json["current_observation"]["visibility_km"];
+                  var humidity = parsed_json["current_observation"]["relative_humidity"];
+                  var wind = parsed_json["current_observation"]["wind_kph"];
                   $("#oculto").each(function() {
                     displaying = $(this).css("display");
                     if(displaying == "block") {
@@ -102,44 +98,41 @@ var mode_temp = $("input[name=temperature]:checked").val();
                       });
                     }
                   });
-              if (mode_temp === "Fahrenheit") {
-              var tempf = parsed_json["current_observation"]["temp_f"];
-              var tempc = parsed_json["current_observation"]["temp_c"];
-                $('.TempBig').append("<p class =\"tempC\">"+ tempf +" °F</p>");
-                $('.TempF').append( "<p>Celsius</p>"+"<p>"+tempc+"°C</p>");
-              } else {
-              var tempc = parsed_json["current_observation"]["temp_c"];
-              var tempf = parsed_json["current_observation"]["temp_f"];
-                $('.TempBig').append("<p class =\"tempC\">"+ tempc +" °C</p>");
-                $('.TempF').append( "<p>Fahrenheit</p>"+"<p>"+tempf+"°F</p>");
-              };
-              $('.TempBig').append("<p class=\"weather senst\">"+weather+"</p>"+"<p class=\"dewpoint senst\">"+dewpoint+"</p>"+"<p class=\"senst\">Feeling Climate</p>");
-              $('.icon').append("<img src= \""+ icon +"\" alt =\"Image of weather\">");
-              $('.location').append("<p class =\"country\">"+ hcountrie +"</p>"+"<h1 class=\"city\">"+hcity+"</h1>"+"<h1 class=\"state\">"+hstate+"</h1>");
-              $('.update').append("<p>"+update+"</p>");
-              $('.visibility_km').append("<p>"+visibility+"    Km</p>");
-              $('.relative_humidity').append("<p>"+humidity+"</p>");
-              $('.wind_string').append( "<p>"+wind+"   KM/h</p>");
-
-          }
+                  if (mode_temp === "Fahrenheit") {
+                  var tempf = parsed_json["current_observation"]["temp_f"];
+                  var tempc = parsed_json["current_observation"]["temp_c"];
+                    $('.TempBig').append("<p class =\"tempC\">"+ tempf +" °F</p>");
+                    $('.TempF').append( "<p>Celsius</p>"+"<p>"+tempc+"°C</p>");
+                  } else {
+                  var tempc = parsed_json["current_observation"]["temp_c"];
+                  var tempf = parsed_json["current_observation"]["temp_f"];
+                    $('.TempBig').append("<p class =\"tempC\">"+ tempc +" °C</p>");
+                    $('.TempF').append( "<p>Fahrenheit</p>"+"<p>"+tempf+"°F</p>");
+                  };
+                  $('.TempBig').append("<p class=\"weather senst\">"+weather+"</p>"+"<p class=\"dewpoint senst\">"+dewpoint+"</p>"+"<p class=\"senst\">Feeling Climate</p>");
+                  $('.icon').append("<img src= \""+ icon +"\" alt =\"Image of weather\">");
+                  $('.location').append("<p class =\"country\">"+ hcountrie +"</p>"+"<h1 class=\"city\">"+hcity+"</h1>"+"<h1 class=\"state\">"+hstate+"</h1>");
+                  $('.update').append("<p>"+update+"</p>");
+                  $('.visibility_km').append("<p>"+visibility+"    Km</p>");
+                  $('.relative_humidity').append("<p>"+humidity+"</p>");
+                  $('.wind_string').append( "<p>"+wind+"   KM/h</p>");
+                } else {
+                    console.log("Has been double click on search");
+                };
+              }
               });
-            };
-          }
+            }
+            }
           });
-        } else{
-            alert("You must enter data to search");
-        };
-
-});
-    $('#reset').click(function() {
-        $("#clcoment1").val("");
-        $("#clcoment2").val("");
-        location.reload();
+            } else{
+                if ($(".error").length===0){
+                    $('.error').append("<p class =\"letererror\">You must enter a valid country and a state ....!!</p>");
+                };
+            };
     });
 });
- //                 if (codeCountrie.length!=0 && parse2!="querynotfound") {
-       //             console.log("hola que hace")
-         //           $('#search').empty()
-         //         } else{
-          //          console.log("esta mejor")
-           //       }
+$('#reset').click(function() {
+    $("#clcoment1").val("");
+    $("#clcoment2").val("");
+    location.reload();
+});
